0797086f7f2ae1ff596440aedf4450818bbf6845
Ishank Dhamagye <100468444+ishankdhamagye@users.noreply.github.com>
2024-08-22 16:13:58 +0000

    Strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,40 @@
+const name = "Ishank";
+const repoCount = 50;
+
+// console.log(name + repoCount + " Value");
+
+// console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+
+const gameName = new String('Ishank-ID-dd');
+// console.log(gameName[0]);
+// console.log(gameName._proto_);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('h'));
+
+const newString = gameName.substring(0,4);
+// console.log(newString); // Isha
+
+const anotherString = gameName.slice(-8,4);
+//console.log(anotherString);
+
+const newStringOne = "    hitesh    ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "http://ishank.com/ishank%20dhamgaye";
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('ishank'));
+// console.log(url.includes('Sunder'));
+
+console.log(gameName.split('-'));
+
+
+
+
+
+
+
+
+
